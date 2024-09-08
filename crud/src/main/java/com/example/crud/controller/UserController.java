f6a742cb68985c36beeeb26e2979dbadb72d968d
PRAVEEN KUMAR V <praveenkumarv989@gmail.com>
2024-09-08 13:18:59 +0530

    first commit

---
+++ crud/src/main/java/com/example/crud/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.crud.controller;
+
+import com.example.crud.model.User;
+import com.example.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(ResponseEntity::ok)
+                   .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userRepository.save(user);
+        return ResponseEntity.ok(createdUser); // Returning the created user
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setAddress(updatedUser.getAddress());
+            userRepository.save(user);
+            return ResponseEntity.ok(user);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
